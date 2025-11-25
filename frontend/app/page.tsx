e158f56be5b7b8cf2c6a0adbda537575d79ebf80
SahanMawadawila <sahansithira12@gmail.com>
2025-11-25 23:48:08 +0530

    resolve merge error

--- frontend/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ApplicationDiagramViewer } from "../components/diagram/application-diagram-viewer";
-export default function Home() {
-  return (
-    <main className="w-full h-screen">
-      <ApplicationDiagramViewer />
-    </main>
-  );
-}
