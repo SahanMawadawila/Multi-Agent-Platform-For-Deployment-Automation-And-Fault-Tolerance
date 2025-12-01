bd3e760248efe7eca6ee4d85a8bd950bcfdeebdc
TGJKKUMARASIRI <jkktg001@gmail.com>
2025-12-01 19:25:13 +0530

    Google GitHub Login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <div>Hello World</div>;
+"use client"
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function AuthButton() {
+  const { data: session } = useSession()
+  console.log(session)
+
+  if (!session) {
+    return (
+      <>
+        <button onClick={() => signIn("google")}>Sign in with Google</button>
+        <button onClick={() => signIn("github")}>Sign in with GitHub</button>
+      </>
+    )
+  }
+
+  return (
+    
+    <button onClick={() => signOut()}>Sign Out</button>
+  )
 }
