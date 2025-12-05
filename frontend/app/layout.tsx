378e8608590d30247b6366556188813fcb921d86
TGJKKUMARASIRI <jkktg001@gmail.com>
2025-12-06 00:30:15 +0530

    merging

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,10 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Providers>
           {children}
         </Providers>
