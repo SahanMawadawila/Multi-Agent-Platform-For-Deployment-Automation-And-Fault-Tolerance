e3b3c57a6da4ef0fb6b0950f6cdd7b01356dbe0e
sahan sithira <147269277+SahanMawadawila@users.noreply.github.com>
2025-12-06 01:20:49 +0530

    Revert "Janani"

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from app.routers import auth
 
 app = FastAPI()
 
-app.include_router(auth.router, prefix="/auth")
-
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello Worlds"}
+@app.get("/")
+async def root():
+    return {"message": "Hello Worlds"}
